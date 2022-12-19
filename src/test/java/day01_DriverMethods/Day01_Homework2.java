46298f3b7a421c52dc9d8c782d4e63e5b9144025
Sumeyye <sumeyyedagdevirozmen@gmail.com>
2022-12-19 13:51:58 +0000

    19/12/22

--- src/test/java/day01_DriverMethods/Day01_Homework2.java
+++ src/test/java/day01_DriverMethods/Day01_Homework2.java
@@ -12,22 +12,33 @@ public class Day01_Homework2 {
         driver.manage().window().maximize();
         //Amazon soyfasina gidelim. https://www.amazon.com/
         driver.get("https://www.amazon.com");
+        Thread.sleep(2000);
 
         //Sayfanin konumunu ve boyutlarini yazdirin
         System.out.println("Sayfanin Konumu: " +driver.manage().window().getPosition()+"\n"+
                 "Sayfanin Boyutu: "+driver.manage().window().getSize());
 
         //Sayfanin konumunu ve boyutunu istediginiz sekilde ayarlayin
-        Dimension d =new Dimension(1000,1000);
+//        driver.manage().window().setSize(new Dimension(10,12));
+//        driver.manage().window().setPosition(new Point(1000,1000));
+//
+        Point p= new Point(10,12);//Konum
+        System.out.println("Sayfanin Yeni Konum Degerleri = " +"("+ p.x+","+p.y+")");
+
+
+        Dimension d =new Dimension(1000,875);//Boyut
         System.out.println("Sayfanin yeni boyutlari = " + "("+d.width+","+d.height+")");
 
-        Point p= new Point(10,12);
-        System.out.println("Sayfanin Yeni Konum Degerleri = " +"("+ p.x+","+p.y+")");
+
+        Thread.sleep(3000);
         driver.manage().window().setPosition(p);
+        Thread.sleep(3000);
         driver.manage().window().setSize(d);
 
         //Sayfanin sizin istediginiz konum ve boyuta geldigini test edin
-        if(d.width==1000 && d.height==1000){
+
+
+        if(d.width==1000 && d.height==875){
             System.out.println("Konum Testi PASSED");
         }else{
             System.out.println("Konum Testi FAILED");
@@ -38,8 +49,25 @@ public class Day01_Homework2 {
         }else{
             System.out.println("Boyut Testi FAILED");
         }
+        System.out.println("Son Konum Degerleri: "+driver.manage().window().getPosition());
+
+        System.out.println("Son Boyut Degerleri: "+driver.manage().window().getSize());
 
         //Sayfayi kapatin
         driver.quit();
     }
 }
+
+/*
+ // 5. Sayfanin sizin istediginiz konum ve boyuta geldigini test edin
+   int x=driver.manage().window().getPosition().getX();
+   int y=driver.manage().window().getPosition().getY();
+   int width=driver.manage().window().getSize().getWidth();
+   int height=driver.manage().window().getSize().getHeight();
+
+   if (x==0 && y==0 && width==500 && height==500){
+       System.out.println("Test PASSED");
+   }else{
+       System.out.println("TEST FAILED");
+   }
+ */
